d0d0b83307d87bf8f896fbacb4a32ebce17c2e94
Alvin Qi <peng.edy@gmail.com>
2017-08-21 02:15:44 +0800

    Add Search Insert Position * just using BinarySearch, which is cool * Empty psvm

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -269,11 +269,26 @@ class Solution {
         StdOut.println(Arrays.toString(nums1));
     }
 
+    /* Search Insert Position
+     * Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+     * You may assume no duplicates in the array.
+     * Here are few examples.
+     *  [1,3,5,6], 5 → 2
+     *  [1,3,5,6], 2 → 1
+     *  [1,3,5,6], 7 → 4
+     *  [1,3,5,6], 0 → 0
+     */
+    static int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return low;
+    }
+
     public static void main(String[] args) {
-        int[] a = {0};
-        int[] b = {1};
-        int m = 0;
-        int n = 1;
-        merge(a,m,b,n);
     }
 }
