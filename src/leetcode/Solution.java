80b1e7b96c8003e663eac44d03c6a20736c8388b
Alvin Qi <peng.edy@gmail.com>
2017-08-22 07:08:34 +0800

    Update

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -480,7 +480,7 @@ class Solution {
         int left = 1, right = Integer.MAX_VALUE;
         while (true) {
             int mid = left + (right - left) / 2;
-            if (mid > x/mid) right = mid - 1;
+            if (mid > x / mid) right = mid - 1;
             else {
                 if (mid + 1 > x / (mid + 1)) return mid;
                 left = mid + 1;
